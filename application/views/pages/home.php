7289089af13f1482acc5d1b4adfffe4a5b11bc15
UshaBisht28 <webusha28@gmail.com>
2024-01-25 14:50:03 +0530

    update

---
+++ application/views/pages/home.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Api Reports</title>
+  <link href="<?php echo site_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo site_url()?>assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- Start:: Header -->
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"><img src="<?php echo base_url()?>assets/image/logo.png" alt=""></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Demo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Documentation
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Json Api Documentation</a></li>
+                <li><a class="dropdown-item" href="#">Pdf Api Documentation</a></li>
+                <li><a class="dropdown-item" href="#">API SDK's</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- End:: Header -->
+
+  <!-- Start:: Banner -->
+  <section class="banner-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-6 col-12">
+          <div class="banner-content">
+            <h1 class="banner-heading">Get Astrology Reports with the Power of Astrology for your Websites and
+              Applications.</h1>
+            <a href="#" class="btn purchase-btn">Purchase Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+  </section>
+  <!-- End:: Banner -->
+
+  <!-- Start:: -->
+  <section>
+    <div class="container">
+      <div class="product-container">
+        <h2 class="product-heading">Products</h2>
+        <div class="product-list-container">
+          <div class="product-list">
+            <div class="item">
+              <img src="assets/image/product-1.png" alt="Product Image" />
+              <h3 class="item-heading">Vedic Astrology</h3>
+              <p class="item-description">Kundali calculations and analysis based on Vedic principles.</p>
+            </div>
+            <div class="item">
+              <h3 class="title">Western Astrology</h3>
+              <p class="desc">Natal &amp; transit horoscope reports using Western Astrology methods.</p>
+            </div>
+            <div class="item">
+              <h3 class="title">Daily Horoscope</h3>
+              <p class="desc">Wide range of personalised and generalised daily predictions.</p>
+            </div>
+            <div class="item">
+              <h3 class="title">PDF Reports</h3>
+              <p class="desc">Single API to get the horoscope in PDF format with customization.</p>
+            </div>
+            <div class="item">
+              <h3 class="title">Tarot Reading</h3>
+              <p class="desc">Kundali calculations and analysis based on Vedic principles.</p>
+            </div>
+            <div class="item">
+              <h3 class="title">Numerology</h3>
+              <p class="desc">Kundali calculations and analysis based on Vedic principles.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End:: -->
+
+
+
+
+ 
+
+
+
+</body>
+<script src="<?php echo site_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+</html>
